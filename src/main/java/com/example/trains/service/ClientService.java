772e645ebb57cdcde7bb729e8d8452b4e33fc4aa
Artyom Krasyuk <artem.krasyuk2004@gmail.com>
2025-03-01 16:44:07 +0400

    security with redis upgrade

--- src/main/java/com/example/trains/service/ClientService.java
+++ src/main/java/com/example/trains/service/ClientService.java
@@ -3,11 +3,9 @@ package com.example.trains.service;
 import com.example.trains.models.Client;
 import com.example.trains.repos.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
