97d324344fe4410d2693cdea70afc1dd4118f688
Artyom Krasyuk <artem.krasyuk2004@gmail.com>
2025-09-05 10:35:27 +0400

    liquibase update

--- src/main/resources/db/changelog/db.changelog-2.3.sql
+++ src/main/resources/db/changelog/db.changelog-2.3.sql
@@ -8,6 +8,10 @@ alter table carriage alter column carriage_id add generated always as identity;
 
 --changeset artyomkrasyuk:3
 alter table carriage_type alter column carriage_type_id add generated always as identity;
+insert into carriage_type(type_name, place_price) values('Плацкарт', 1500);
+insert into carriage_type(type_name, place_price) values('Купе', 3000);
+insert into carriage_type(type_name, place_price) values('СВ', 6000);
+insert into carriage_type(type_name, place_price) values('Сидячий', 300);
 
 --changeset artyomkrasyuk:4
 alter table city alter column city_id add generated always as identity;
